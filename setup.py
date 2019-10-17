0bb3c505341ab0bb582955cec6992861c22cb687
juliettemartin <juliettemart1@gmail.com>
2019-10-17 11:33:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,11 @@ import sysconfig
 
 extra_compile_args = sysconfig.get_config_var('CFLAGS').split()
 extra_compile_args += ['-D', 'AS_PYTHON_EXTENSION', '-std=c99', '-pedantic']
-#extra_link_args = ['-L/home/julia/IBCP/modules/lib/python3.6/site-packages/']
 module1 = Extension('ccmap',
                     libraries = ['m'],
                     include_dirs = ['./include', '../modules/include/python3.6m/'],
                     sources = ['ccmapmodule.c', './src/mesh.c', './src/transform_mesh.c', './src/decoygen.c','./src/encode.c'],
                     extra_compile_args=extra_compile_args
-		    #extra_link_args=extra_link_args
 		    #extra_compile_args=['-D', 'DEBUG', '-D', 'AS_PYTHON_EXTENSION', '-std=c99', '-pedantic'])
 		)
 
