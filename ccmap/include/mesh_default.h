f7c4578fa2dc738a451f9cda3a40e6d16ef9f942
glaunay <pitooon@gmail.com>
2022-07-29 09:03:50 +0200

    DEBUGING sasa

--- ccmap/include/mesh_default.h
+++ ccmap/include/mesh_default.h
@@ -3,8 +3,8 @@
 #define MESH_DEFAULT_H
 
 
-#ifndef _USE_MATH_DEFINES
-    #define _USE_MATH_DEFINES
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
 #endif
 #include <stdio.h>
 #include <stdlib.h>
